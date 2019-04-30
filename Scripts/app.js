2d590025093413abe9786a2afc4d88268822ac38
Vipulgoyal647 <Vipulgoyal647@gmail.com>
2019-04-30 16:03:47 -0400

    Added IIFe to App.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,15 @@
 /* Custom JS goes here */
+
+// IIFE _ Immediately Invoked Function Expression
+// Anonymous Self Executing Function
+(function(){
+
+    function Start()
+    {
+        console.log(" App Started ");
+        console.log("-----------------------------------")
+        console.log(" Hello World ")
+    }
+    window.onload=Start;
+
+})();
