30907d4d5be3da291ed39013ebac9924a3eeb476
Vipulgoyal647 <Vipulgoyal647@gmail.com>
2019-04-30 17:19:08 -0400

    Added About page And Title Detection

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,13 +3,19 @@
 // IIFE _ Immediately Invoked Function Expression
 // Anonymous Self Executing Function
 (function(){
-
+    //Local variable to Detect Page Title
+    var title = document.title;
     function Start()
-    {
+    { 
         console.log(" App Started ");
-        console.log("-----------------------------------")
-        console.log(" Hello World ")
+        console.log("-----------------------------------");
+        console.log(" Hello World ");
+        console.log("-----------------------------------");
+        //Printing Title in Console
+        console.log("Title: "+title);
     }
     window.onload=Start;
+    
+
 
 })();
